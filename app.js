127f002ad3692417f9f3e73055bbb4062d599853
JavieraMunita <95220695+JavieraMunita@users.noreply.github.com>
2022-04-06 22:45:04 -0400

    Primeras funciones modularizadas

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+const index = require('./index')
+
+
+readline.question(`Ingresa una ruta:`, (route) => {
+  index.fileRoute(route)
+  index.verifyExistance(route)
+  index.kindOfRoute(route) 
+})
+
+
+
+
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs')
+const path = require('path')
+const { exit } = require('process')
+
+//console.log("Ingresa una ruta") 
+//process.stdout.write("Ingresa una ruta") Cuál es la diferencia?
+
+const fileRoute = (route) => {
+  const fileAbsolute = path.resolve(route)
+  console.log(`La ruta absoluta es ${fileAbsolute}`)
+}
+
+const verifyExistance = (route) => {
+  const fileExist = fs.existsSync(route) 
+  if (!fileExist) {
+    console.log('Esta ruta no existe')
+    exit()
+  }}
+
+const kindOfRoute = (route) => {
+  const typeOfArchive = fs.statSync(route)
+      if (typeOfArchive.isFile()){
+      console.log('El archivo existe')
+      checkExtension(route)
+    } if (typeOfArchive.isDirectory()) {
+      console.log('El directorio existe')
+    }
+}
+
+const checkExtension = (route) => {
+if (path.extname(route) !== '.md') {
+  console.log('El archivo no es de tipo .md')
+  exit()
+}  else {
+  console.log('El archivo es de tipo .md') 
+} 
+}
+
+module.exports.fileRoute = fileRoute;
+module.exports.verifyExistance = verifyExistance;
+module.exports.kindOfRoute = kindOfRoute;
+module.exports.checkExtension = checkExtension;

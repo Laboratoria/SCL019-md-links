17fc16b21353d8f3d5553954adbadfc359cdc4f3
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-04-12 19:43:16 -0400

    Identifica si es archivo o directorio/ valida si la extension es .md/ si la ruta existe o no

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 let fs = require('fs');
 const path = require('path');
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-readline.question('Ingresa la ruta: ', route => { // input que solicita al usuario ingresar el arcgivo
-  const ruta = path.resolve(route);
-  console.log(`la ruta del archivo es: ${ruta}`); //Muestra la ruta del archivo
-  fs.readFile(route, (err, route) => { 
-    if(err) {
-      console.log('error: ', err);
-    } else {
-      console.log( route.toString()); //Muestra el contenido del archivo en la consola
-    }
-  });
-  readline.close();
-});
+
 
 //-----Determinar si la ruta es absoluta -------
 const pathAbsolute = (ruta)=>{
-  path.isAbsolute(ruta);
-  console.log(`la ruta es absoluta ${pathAbsolute}`);
+  const absolute = path.isAbsolute(ruta);
+  if (absolute) {
+    console.log(`la ruta es absoluta`);
+  }else {
+    console.log(`La ruta es relativa, será convertida en absoluta.`);
+    path.resolve(ruta);
+
+  }
 } 
-  
+//---- Verifica si la ruta existe o no ----
+const routeExist = (ruta)=> fs.existsSync(ruta);
+
+//------- Verificar que es archivo o directorio
+const archiDirectory= (ruta) => fs.statSync(ruta);
+
+//--- Saber la extension del archivo
+const extension= (ruta) => path.extname(ruta);
+
+//--- Leer archivos dentro del directo 
+const readDirectory = (ruta) => fs.readdirSync(ruta);
+
+module.exports.pathAbsolute = pathAbsolute;
+module.exports.routeExist = routeExist;
+module.exports.archiDirectory = archiDirectory;
+module.exports.extension = extension;
+module.exports.readDirectory = readDirectory;
 
 /*----permite verificar si un archivo o carpeta existe
 
--- index.js
+++ index.js
@@ -1,3 +1,68 @@
 const app = require('./app.js');
 const path = require('path');
+const { exit } = require('process')
+
+let fs = require('fs');
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+readline.question('Ingresa la ruta: ', route => { // input que solicita al usuario ingresar el archivo
+    // console.log(`la ruta del archivo es: ${ruta}`); //Muestra la ruta del archivo
+    app.pathAbsolute(route);   
+    const routeExist = app.routeExist(route)
+    if (routeExist == false){
+        console.log ("No existe la ruta")
+        // readline.close();
+        exit();
+    }
+    else {
+    console.log("La ruta existe:", routeExist);
+    }
+
+    const archiDirectory = app.archiDirectory(route);
+
+    if (archiDirectory.isFile()){
+        console.log('Es archivo');
+        const extension = app.extension(route);
+        if (extension == '.md'){
+            console.log('Es un archivo .md');
+        } else{
+            console.log('No es un archivo .md');
+            exit();
+        }
+    }
+
+    if (archiDirectory.isDirectory()){
+        const arrayMd = [];
+        const readDirectory = app.readDirectory(route).forEach(files => {
+            const mdExtension = app.extension(files)
+            if (mdExtension == '.md') {
+                // console.log(files);
+                arrayMd.push(files);
+            } 
+        });
+        
+        if (arrayMd == ''){
+            console.log('Este directorio no contiene archivos .md');
+            exit();
+        }else{
+            console.log('Es directorio contiene archivos con la extesion .md');
+            console.log(arrayMd);
+
+        }
+        // const mdFiles = readDirectory.
+    }
+
+    // fs.readFile(route, (err, route) => { 
+    //     if(err) {
+    //     console.log('error: ', err);
+    //   } else {
+    //     console.log( route.toString()); //Muestra el contenido del archivo en la consola
+    //   }
+    // });
+    readline.close();
+  });
 
